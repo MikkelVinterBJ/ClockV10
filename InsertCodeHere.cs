6e575801f56ee05949e7deec17114cea94eaafd8
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-03-30 13:40:25 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClockV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+             Clock myClock = new Clock(13,59);
+
+            Console.WriteLine(myClock.Display);
+            myClock.AdvanceOneMinute();
+            Console.WriteLine(myClock.Display);
+            myClock.AdvanceOneMinute();
+            Console.WriteLine(myClock.Display);
+
+            myClock.Hours = 23;
+            myClock.Minutes = 59;
+
+            Console.WriteLine(myClock.Display);
+            myClock.AdvanceOneMinute();
+            Console.WriteLine(myClock.Display);
+            myClock.AdvanceOneMinute();
+            Console.WriteLine(myClock.Display);
+        }
+    }
+}
